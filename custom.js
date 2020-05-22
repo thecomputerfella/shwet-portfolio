0d8b94dcc14a55572ec50e4404b32aae72ddb51a
thecomputerfella <shwet.mishra123@gmail.com>
2020-05-22 09:02:54 +0530

    major updates

--- custom.js
+++ custom.js
@@ -1,5 +1,23 @@
-$('#articles_navbar').click(function(){
-    $('#content').load('articles.html');
 
-    $('#container').hide('index.html');
-});
+
+// function myFunction(event) { 
+//     console.log(event.target.nodeName);
+// }
+
+// OPEN LOGIN MODAL
+var openModal = document.getElementById("login-btn");
+openModal.addEventListener('click',function(){
+    console.log("login tap");
+    document.getElementById("login-modal").style.display = "flex";
+    event.preventDefault();
+})
+//CLOSE LOGIN MODAL
+function closeModal(){
+    console.log("close tap");
+    document.getElementById("login-modal").style.display = "none";
+    event.preventDefault();
+}
+
+//LOGIN VALIDATION
+
+
